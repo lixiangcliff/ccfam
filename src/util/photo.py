65fedfd1a1969b3fad784a37b47ec3f31a03812c
lixiangcliff <lixiang.cliff@gmail.com>
2017-03-02 23:16:42 -0800

    embed video

---
+++ src/util/photo.py
@@ -0,0 +1,18 @@
+import PIL.Image
+import PIL.ExifTags
+
+img = PIL.Image.open('/Users/Cliff/Downloads/6390788212880572933.jpg')
+exif_data = img._getexif()
+
+print (exif_data)
+
+exif = {
+    PIL.ExifTags.TAGS[k]: v
+    for k, v in img._getexif().items()
+    if k in PIL.ExifTags.TAGS
+}
+
+print ("#################")
+
+for k, v in exif.items():
+    print(k, v)

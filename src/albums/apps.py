5d739f0c233e9e087a64e16b37e71bd53809955a
lixiangcliff <lixiang.cliff@gmail.com>
2017-04-08 12:05:29 -0700

    replace hard-coded path

--- src/albums/apps.py
+++ src/albums/apps.py
@@ -1,13 +1,5 @@
 from django.apps import AppConfig
-from django.db.models.signals import post_migrate
-
-# def do_stuff(sender, **kwargs):
-#     mymodel = sender.get_model('Album')
-#     mymodel.objects.get() # etc...
 
 
 class AlbumsConfig(AppConfig):
     name = 'albums'
-
-    # def ready(self):
-    #     post_migrate.connect(do_stuff, sender=self)

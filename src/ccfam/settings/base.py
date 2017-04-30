5a0a799688870a89861aac99148a5bd9ac183f61
lixiangcliff <lixiang.cliff@gmail.com>
2017-04-29 20:13:35 -0700

    put db setting in base

--- src/ccfam/settings/base.py
+++ src/ccfam/settings/base.py
@@ -101,6 +101,18 @@ WSGI_APPLICATION = 'ccfam.wsgi.application'
 #     }
 # }
 
+if 'RDS_DB_NAME' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': os.environ['RDS_DB_NAME'],
+            'USER': os.environ['RDS_USERNAME'],
+            'PASSWORD': os.environ['RDS_PASSWORD'],
+            'HOST': os.environ['RDS_HOSTNAME'],
+            'PORT': os.environ['RDS_PORT'],
+        }
+    }
+
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
 

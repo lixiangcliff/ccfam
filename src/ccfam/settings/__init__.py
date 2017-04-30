390d4e60b1afacbc3cf48012bf704a217b1bf216
lixiangcliff <lixiang.cliff@gmail.com>
2017-04-29 23:33:09 -0700

    debug

--- src/ccfam/settings/__init__.py
+++ src/ccfam/settings/__init__.py
@@ -1,18 +1,18 @@
-from .prod import *
+from .base import *
 
-# try:
-#     from .prod import *
-# except:
-#     pass
+try:
+    from .dev import *
+except:
+    pass
 
-# try:
-#     from .dev import *
-# except:
-#     pass
-#
-# try:
-#     from .vm import *
-# except:
-#     pass
+try:
+    from .vm import *
+except:
+    pass
 
+try:
+    from .prod import *
+except:
+    pass
 
+# i'm guessing the latter one will overwrite previous one

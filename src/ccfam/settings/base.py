da7cd8ee90be82f23d3ea53cf757db0bdd888db4
lixiangcliff <lixiang.cliff@gmail.com>
2017-05-02 15:49:10 -0700

    set up db config

--- src/ccfam/settings/base.py
+++ src/ccfam/settings/base.py
@@ -84,12 +84,12 @@ WSGI_APPLICATION = 'ccfam.wsgi.application'
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
 # sqlite for dev
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 # mysql db for prod
 # DATABASES = {
